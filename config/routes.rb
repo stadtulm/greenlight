9b759446a462ae91c15e7caafa176a19444b7b5e
Sebastian Fischer <sebastian@fischerulm.de>
2020-04-21 22:18:10 +0200

    impressum: added

--- config/routes.rb
+++ config/routes.rb
@@ -23,6 +23,9 @@ Rails.application.routes.draw do
   match '/401', to: 'errors#unauthorized', via: :all, as: :unauthorized
   match '/404', to: 'errors#not_found', via: :all, as: :not_found
   match '/500', to: 'errors#internal_error', via: :all, as: :internal_error
+  
+  # ulmlernt specifics
+  get '/impressum', to: 'main#impressum'
 
   # Signin/Signup routes.
   get '/signin', to: 'sessions#signin', as: :signin
